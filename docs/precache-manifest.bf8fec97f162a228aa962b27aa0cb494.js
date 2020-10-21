f8f2c030b7e24a794edc43b8a9f2db35d0648e84
oliver <oliver.kali.hub@gmail.com>
2020-10-21 14:28:35 -0500

    Se hizo un cambio de nombre a la carpeta build

---
+++ docs/precache-manifest.bf8fec97f162a228aa962b27aa0cb494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32be7a2c048678d70dca3b307fda4722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13b7e6233f4401a11b04",
+    "url": "/static/css/main.d6971bda.chunk.css"
+  },
+  {
+    "revision": "6c1d956ed848b04fbeb2",
+    "url": "/static/js/2.ec823eca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ec823eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b7e6233f4401a11b04",
+    "url": "/static/js/main.db921b04.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
